d7e087d32d10f384ca45b4a319aa55b34c523865
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-16 21:40:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str[0]!==str[1]){
+		return str[0];
+	}
 	for(let i=0;i<str.length-1;i++){
 		if(str[i-1]!==str[i] and str[i]!==str[i+1]){
 			return str[i];
 		}
 	}
+	if(str[str.length-2]!==str[str.length-1]){
+		return str[str.length-2];
+	}
 	return null;
 }
-// const input = prompt("Enter a string");
+const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
